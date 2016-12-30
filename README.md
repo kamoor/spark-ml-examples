21508388415ee0b95cb70fe5f3f261146554f862
Kamoor <rkamoor@gmail.com>
2016-12-30 15:47:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+This project contains spark-ml algorithms examples
+
+Setup
+====
+Install scala 
+Install spark 
+
+
+How to Run
+=========
+1. Check out the code
+2. mvn clean install
+3. spark-submit --class <class-youwant-to-run> target/spark-ml-examples-1.0-SNAPSHOT.jar <parameters>
+ See each code header to learn more about parameters needed. 
---
+++ src/main/scala/com/kamoor/examples/spark/ml/statistics/Correlation.scala
@@ -0,0 +1,54 @@
+package com.kamoor.examples.spark.ml.statistics
+
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+
+ * This sample code shows how to find pearson and spearman correlation of a set of co-ordinates.
+ * Pass an s3 or hdfs location of the input file contains  one x,y pair per line
+ *
+ * To Submit:
+ * spark-submit --class com.kamoor.examples.spark.ml.statistics.Correlation spark-ml-examples-1.0-SNAPSHOT.jar s3://dp-emr-general/kamoor/xy.txt
+ *
+ * Sample xy.txt in resources/test-files/xy.txt
+ *
+ */
+
+class CorrelationProcessor(sc: SparkContext) {
+
+
+
+  def run(input: String) = {
+
+    val xy = sc.textFile(input).map(line => line.split(","));
+    xy.take(5);
+
+    val xD = xy.map(v1 => v1(0).toDouble)
+    val yD = xy.map(v1 => v1(1).toDouble)
+
+    val corrPearson: Double = Statistics.corr(xD, yD, "pearson")
+    val corrSpearman: Double = Statistics.corr(xD, yD, "spearman")
+
+    println("---------------------------------- ")
+    println("Pearson correlation  "+ corrPearson)
+    println("Spearman correlation "+ corrPearson)
+    println("---------------------------------- ")
+  }
+
+}
+
+object Correlation {
+
+  def main(args: Array[String]) {
+    val input = args(0)
+    println("Starting correlation calculation for "+ input)
+
+    val conf = new SparkConf().setAppName("CorrelationKJob").setMaster("local")
+    val context = new SparkContext(conf)
+    val correlationProcessor = new CorrelationProcessor(context)
+    correlationProcessor.run(input)
+    context.stop()
+    println("Ending Job");
+  }
+}
